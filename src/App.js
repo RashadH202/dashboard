c9edb31ebae9b6dcc0a87654c66bfcbeaf9885a1
Rashadtheone <rashad.hammonds0114@gmail.com>
2024-04-29 13:28:35 -0400

    adding db

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
 import './App.css';
-import {Amplify} from 'aws-amplify'
-import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify, API, graphqlOperation} from 'aws-amplify'
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
+import { useEffect,useState } from 'react';
+
+
 
 
 Amplify.configure(awsExports);
 
 function App() {
+  const [todo, setTodo] = useState([])
+
+  useEffect(() => {
+    fetchtodo()
+  },[])
+
+  const fetchtodo = async () => {
+try {
+  const todoData = await API.graphql(graphqlOperation(listTodos))
+  const todoList = todoData.data.listTodo.item
+  console.log('song list', todoList)
+  setTodo(todoList)
+
+} catch(error){
+  console.log('error on fetching song', error)
+}
+  } 
   return (
     <div className="App">
    <h1>Hello</h1>
+   <AmplifySignOut />
   </div>
   );
 }
